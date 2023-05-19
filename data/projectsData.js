e819f669ff03893a88ed3b9931440b7b7b3d8f87
rpatel117 <129987028+rpatel117@users.noreply.github.com>
2023-05-19 13:07:18 -0500

    Add files via upload

---
+++ data/projectsData.js
@@ -0,0 +1,18 @@
+const projectsData = [
+  {
+    title: 'Caleo',
+    description: `A web app to convert text into calendar event files`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://dev.d3cjmkp9wsw0py.amplifyapp.com/',
+  },
+  {
+    title: 'The Time Machine',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData
